59727eb4e4355f511fae9ad6f981bc8be7074272
萝卜馋青菜 <fast_ant@163.com>
2020-11-24 19:39:52 +0800

    remove target
    Signed-off-by: 萝卜馋青菜 <fast_ant@163.com>

---
+++ tool/src/main/java/org/py/tool/file/PathUtil.java
@@ -0,0 +1,31 @@
+package org.py.tool.file;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class PathUtil {
+
+    @Test
+    public void getDirectByBasePath() {
+        String basePath = "/Users/pengyue.du/Code/Meijia/Work01/learn_test";
+        String direct = "target";
+
+        File file = new File(basePath);
+        findDirect(file, direct, basePath);
+    }
+
+    private void findDirect(File file, String direct, String removePath) {
+        if(file.isDirectory()) {
+            if(file.getName().equals(direct)) {
+                String targetPath = file.getAbsolutePath().substring(removePath.length() + 1);
+                System.out.println(String.format("git rm --cached -r %s;", targetPath));
+            }
+            File[] files = file.listFiles();
+            for(File subFile : files) {
+                findDirect(subFile, direct, removePath);
+            }
+        }
+    }
+
+}
